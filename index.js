c21efa814513149020d4443e2c849a5cc4849274
MonEvade <168145856+Monmon9ne@users.noreply.github.com>
2026-01-13 18:32:24 +0700

    Add POST endpoint and JSON middleware to index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Render");
+app.use(express.json()); // ⚠️ BẮT BUỘC
+
+app.post("/", (req, res) => {
+	const { jobId, pets } = req.body;
+
+	if (!jobId || !pets) {
+		return res.status(400).json({ ok:false });
+	}
+
+	console.log("JOB:", jobId);
+	console.log("PETS:", pets);
+
+	res.json({ ok:true });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+	console.log("MAKING THIS THING FOR THE BOT SO I CAN MAKE A AJ MENU", PORT);
+});
